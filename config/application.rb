c366c95ed1b0da85cebb8d0318e3942a543746ba
marvinjason <marvinjason.sy@gmail.com>
2017-05-04 01:40:16 +0800

    Initial commit

---
+++ config/application.rb
@@ -0,0 +1,29 @@
+require_relative 'boot'
+
+require "rails"
+# Pick the frameworks you want:
+require "active_model/railtie"
+require "active_job/railtie"
+# require "active_record/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_view/railtie"
+require "action_cable/engine"
+require "sprockets/railtie"
+require "rails/test_unit/railtie"
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module CsConferenceMessengerBot
+  class Application < Rails::Application
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+    
+    # Auto-load the bot and its subdirectories
+	config.paths.add File.join('app', 'bot'), glob: File.join('**', '*.rb')
+	config.autoload_paths += Dir[Rails.root.join('app', 'bot', '*')]
+  end
+end
