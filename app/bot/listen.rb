fc265b4d3fbfe81d6d807b17e03f11742f335ace
marvinjason <marvinjason.sy@gmail.com>
2017-05-04 23:00:11 +0800

    Updated postback

--- app/bot/listen.rb
+++ app/bot/listen.rb
@@ -102,13 +102,13 @@ Bot.on :postback do |postback|
 	case postback.payload
 	when 'venue'
 		puts '-' * 30
-		puts postback.recipient['id']
+		puts postback.recipient['id'].class
 		puts postback.recipient[:id]
 		puts '-' * 30
 		Bot.deliver(
 			{
 				recipient: {
-					id: postback.recipient['id']
+					id: postback.recipient['id'].to_s
 				},
 				message: {
 					attachment: {
