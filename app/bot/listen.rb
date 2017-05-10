b38102cacb3d560c222fadf840108a0eac3ef90c
marvinjason <marvinjason.sy@gmail.com>
2017-05-11 00:10:12 +0800

    Updated get started setup

--- app/bot/listen.rb
+++ app/bot/listen.rb
@@ -8,85 +8,43 @@ Facebook::Messenger::Profile.set({
 
 Facebook::Messenger::Subscriptions.subscribe(access_token: ENV["ACCESS_TOKEN"])
 
-Bot.on :message do |message|
-
-	greetings = %w(hi hello hey ui um)
-	queries = %w(question tanong ask)
-	venue = %w(where saan venue)
-	time = %w(when kailan time date)
-	end_note = ['i see', 'ok', 'okie', 'haha', 'thanks', 'salamat', 'ohhh', ':)', ':3', '^_^', 'XD']
-
-	collection = greetings + venue + time + direction + end_note
-	fuzzy_match = FuzzyMatch.new(collection).find(message.text)
-
-	puts collection
-	puts '-' * 30
-	puts fuzzy_match
-	puts '-' * 30
-
-	if greetings.include?(fuzzy_match)
-		message.reply(
-			text: 'Hey there! ;)'
-		)
-	elsif queries.include?(fuzzy_match)
-		message.reply(
-			text: 'Go ahead and ask me a question. :P'
-		)
-	elsif venue.include?(fuzzy_match)
-		message.reply(
-			text: 'The event is happening here!'
-		)
-
-		message.reply(
-			attachment: {
-		    type: 'template',
-		    payload: {
-		    	template_type: 'generic',
-		    	elements: [
-		    		{
-		    			title: 'FEU Institute of Technology',
-		    			subtitle: 'P. Paredes, Sampaloc, Manila',
-		    			image_url: 'http://68.media.tumblr.com/6c6bb66539853c18aed9a42f8bb113bf/tumblr_inline_nb7v60Ex7D1sfo6p3.jpg',
-		    			default_action: {
-		    				type: 'web_url',
-		    				url: 'https://maps.google.com/maps?ll=14.604161,120.988607&z=16&t=m&hl=en-US&gl=PH&mapclient=embed&cid=12487658003456722574'
-		    			}
-		    		}
-	    		]
-		    }
-		  }
-		)
-	elsif time.include?(fuzzy_match)
-		message.reply(
-			text: 'The event starts at 1PM and ends at 6PM on June 6, 2017.'
-		)
-	elsif end_note.include?(fuzzy_match)
-		message.reply(
-			text: '^_^'
-		)
-	else
-		message.reply(
-			text: %(Sorry, I didn't quite catch that :/)
-		)
-		message.reply(
-			attachment: {
-		    type: 'template',
-		    payload: {
-		      template_type: 'button',
-		      text: %(Maybe you meant to ask a question regarding one of these?),
-		      buttons: [
-		        { type: 'postback', title: 'Venue', payload: 'venue' },
-		        { type: 'postback', title: 'Date & Time', payload: 'datetime' },
-		        { type: 'postback', title: 'CS Conference 2017', payload: 'about' }
-		      ]
-		    }
-		  }
-		)
-	end
-end
-
 Bot.on :postback do |postback|
 	case postback.payload
+	when 'GET_STARTED_PAYLOAD'
+		Bot.deliver(
+			{
+				recipient: {
+					id: postback.sender['id']
+				},
+				message: {
+					text: %Q(Hi #{postback.sender}, thanks for your message. We are not here right now, but we'll get back to you soon!)
+				}
+			},
+			access_token: ENV['access_token']
+		)
+
+		Bot.deliver(
+			{
+				recipient: {
+					id: postback.sender['id']
+				},
+				message: {
+					attachment: {
+						type: 'template',
+						payload: {
+							template_type: 'button',
+							text: %(In the meantime, you might want to check these out:),
+							buttons: [
+								{ type: 'postback', title: 'Venue', payload: 'venue' },
+								{ type: 'postback', title: 'Date & Time', payload: 'datetime' },
+								{ type: 'postback', title: 'CS Conference 2017', payload: 'about' }
+							]
+						}
+					}
+				}	
+			}, 
+			access_token: ENV['access_token']
+		)
 	when 'venue'
 		Bot.deliver(
 			{
@@ -95,23 +53,23 @@ Bot.on :postback do |postback|
 				},
 				message: {
 					attachment: {
-				    type: 'template',
-				    payload: {
-				    	template_type: 'generic',
-				    	elements: [
-				    		{
-				    			title: 'FEU Institute of Technology',
-				    			subtitle: 'P. Paredes, Sampaloc, Manila',
-				    			image_url: 'http://68.media.tumblr.com/6c6bb66539853c18aed9a42f8bb113bf/tumblr_inline_nb7v60Ex7D1sfo6p3.jpg',
-				    			default_action: {
-				    				type: 'web_url',
-				    				url: 'https://maps.google.com/maps?ll=14.604161,120.988607&z=16&t=m&hl=en-US&gl=PH&mapclient=embed&cid=12487658003456722574'
-				    			}
-				    		}
-			    		]
-				    }
-				  }
-				}	
+						type: 'template',
+						payload: {
+							template_type: 'generic',
+							elements: [
+								{
+									title: 'FEU Institute of Technology',
+									subtitle: 'P. Paredes, Sampaloc, Manila',
+									image_url: 'http://68.media.tumblr.com/6c6bb66539853c18aed9a42f8bb113bf/tumblr_inline_nb7v60Ex7D1sfo6p3.jpg',
+									default_action: {
+										type: 'web_url',
+										url: 'https://maps.google.com/maps?ll=14.604161,120.988607&z=16&t=m&hl=en-US&gl=PH&mapclient=embed&cid=12487658003456722574'
+									}
+								}
+							]
+						}
+					}
+				}
 			},
 			access_token: ENV['ACCESS_TOKEN']
 		)
