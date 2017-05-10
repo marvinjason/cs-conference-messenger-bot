5db1eabe72d08ac0db25dc2d1b6aabe3cde02ba3
marvinjason <marvinjason.sy@gmail.com>
2017-05-11 01:44:51 +0800

    Updated get started setup

--- app/bot/listen.rb
+++ app/bot/listen.rb
@@ -19,7 +19,7 @@ Facebook::Messenger::Profile.set({
   persistent_menu: [
     {
       locale: 'default',
-      composer_input_disabled: true,
+      composer_input_disabled: false,
       call_to_actions: [
         {
           title: 'Options',
