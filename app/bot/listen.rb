9c6b22ad0f2e0c5017c068a7667ffba23140464e
marvinjason <marvinjason.sy@gmail.com>
2017-05-10 22:57:52 +0800

    Updated get started setup

--- app/bot/listen.rb
+++ app/bot/listen.rb
@@ -1,10 +1,11 @@
 include Facebook::Messenger
 
-# Facebook::Messenger::Profile.set({
-#   get_started: {
-#     payload: 'GET_STARTED_PAYLOAD'
-#   }
-# }, access_token: ENV['ACCESS_TOKEN'])
+# Facebook::Messenger::
+Profile.set({
+  get_started: {
+    payload: 'GET_STARTED_PAYLOAD'
+  }
+}, access_token: ENV['ACCESS_TOKEN'])
 
 # Facebook::Messenger::
 Subscriptions.subscribe(access_token: ENV["ACCESS_TOKEN"])
